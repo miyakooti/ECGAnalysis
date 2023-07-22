e7fe6c0b4f438f3f48e55056ce0541e597afd946
arai <niimaru09@gmail.com>
2023-07-22 21:55:50 +0900

    HRVパラメータの出力部分

--- Constants.py
+++ Constants.py
@@ -2,3 +2,5 @@ class Constants:
 
     subject_name_list = ["kumakura","kim","souma","fujii","tubota","toki"]
     phase_name_list = ["rest", "boredom", "flow", "ultra", "overload"]
+    
+    fs = 500.0
