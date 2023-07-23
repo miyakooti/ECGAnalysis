de6d90e8dbbea515c340946161363e1e149c77cd
arai <niimaru09@gmail.com>
2023-07-23 14:17:55 +0900

    python部分は自動化完了！

--- Constants.py
+++ Constants.py
@@ -1,15 +1,14 @@
 class Constants:
 
+    ### 分析結果格納フォルダ
+    folder_name = "analysis_folder_1"
+
     # データセット作成フェーズ
     subject_name_list = ["kumakura","kim","souma","fujii","tubota","toki"]
     phase_name_list = ["rest", "boredom", "flow", "ultra", "overload"]
     fs = 500.0
     
     # 分析フェーズ
-    
-    ### 分析結果格納フォルダ
-    folder_name = "0723_introduce_mkdir"
-    
     target_columns = ["bpm", "ibi", "lf", "hf","lf/hf","kubios_lf","kubios_hf","kubios_lf/hf","sdsd","rmssd","pnn20","pnn50","concentration_FC3","concentration_FC4","concentration_FCz","concentration_mean","FC3","FC4","FCz","mean_3ch","lf_diff", "hf_diff","lfhf_diff","hr_mad","sd1","sd2","s","sd1/sd2","breathingrate","lf_nu","hf_nu"]
     target_phases = ["boredom","flow","ultra","overload"]
     survey_items = ["difficulty","fast","fulfillment","absorb","questionnaire_average"]
--- Makefile
+++ Makefile
@@ -1,5 +1,4 @@
 dataflow:
-
 	
 	# 心電図
 	echo "prepare dataset"
@@ -17,8 +16,6 @@ dataflow:
 	jupyter execute regression_of_flow_score.ipynb
 
 	
-	
-	
 debug:
 	read -p "Enter the value: " input_value
 	# 環境変数に値を設定する
@@ -27,3 +24,22 @@ debug:
 
 
 
+## Makefile内での入力と環境変数への格納
+#
+#SHELL := /bin/bash
+#
+## デフォルト値を設定
+#folder_name ?= default_folder
+#
+## Makefileのターゲット
+#set_folder_name:
+#    @read -p "Enter the folder name [$(folder_name)]: " input_val; \
+#    export FOLDER_NAME=$${input_val:-$(folder_name)}; \
+#
+## 例として新しいフォルダを作成するターゲットを追加
+#create_folder:
+#	@mkdir -p $(FOLDER_NAME)
+#	@echo "Folder '$(FOLDER_NAME)' created successfully."
+#
+## 新しいフォルダを作成するターゲットを呼び出すターゲットを追加
+#run: set_folder_name create_folder
