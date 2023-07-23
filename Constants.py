e152a32db071453c1f8acc01074334d674edec2e
arai <niimaru09@gmail.com>
2023-07-23 13:22:06 +0900

    make path_to_matlab_repository_folder constant

--- Constants.py
+++ Constants.py
@@ -13,3 +13,5 @@ class Constants:
     target_columns = ["bpm", "ibi", "lf", "hf","lf/hf","kubios_lf","kubios_hf","kubios_lf/hf","sdsd","rmssd","pnn20","pnn50","concentration_FC3","concentration_FC4","concentration_FCz","concentration_mean","FC3","FC4","FCz","mean_3ch","lf_diff", "hf_diff","lfhf_diff","hr_mad","sd1","sd2","s","sd1/sd2","breathingrate","lf_nu","hf_nu"]
     target_phases = ["boredom","flow","ultra","overload"]
     survey_items = ["difficulty","fast","fulfillment","absorb","questionnaire_average"]
+    
+    path_to_matlab_repository_folder = "/Users/miyakooti/repositories/arai_MATLAB_program"
