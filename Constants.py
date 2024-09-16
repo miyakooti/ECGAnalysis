c6bcb30d01022de080db479b8b2ae043ed852c9d
arai <niimaru09@gmail.com>
2024-09-17 01:28:36 +0900

    anova, 最小値計算

--- Constants.py
+++ Constants.py
@@ -1,7 +1,7 @@
 class Constants:
 
     ### 分析結果格納フォルダ
-    folder_name = "20240622_kyoto"
+    folder_name = "20240627_kyoto"
 
     # データセット作成フェーズ
     subject_name_list = ["kumakura","kim","souma","fujii","tubota","toki","mori","hochi","yamada","takei","tenshin","masanori","enoki","ito","yoshioka","chizuru"]
@@ -11,7 +11,7 @@ class Constants:
     
     # 分析フェーズ
     target_columns = ["bpm", "ibi", "lf", "hf","lf/hf","kubios_lf","kubios_hf","kubios_lf/hf","sdsd","rmssd","pnn20","pnn50","concentration_FC3","concentration_FC4","concentration_FCz","concentration_mean","FC3","FC4","FCz","mean_3ch","lf_diff", "hf_diff","lfhf_diff","hr_mad","sd1","sd2","s","sd1/sd2","breathingrate","lf_nu","hf_nu"]
-    target_phases = ["boredom","flow","ultra","overload"]
+    target_phases = ["rest", "boredom","flow","ultra","overload"]
     survey_items = ["difficulty","fast","fulfillment","absorb","questionnaire_average"]
     
     path_to_matlab_repository_folder = "/Users/miyakooti/repositories/arai_MATLAB_program"
